f51c44e6809c1db5e6146daf2134f510559ee8b9
Kortney <kortney@iMac.local>
2022-02-11 21:37:07 -0400

    wrote out steps to writing code in plain english

---
+++ script.js
@@ -0,0 +1,8 @@
+//display a pokemon image
+//take the users input
+//save it and then compare it against the correct answer
+//if its right, score goes up and shows you the pokemon & name, adds to "correct array"
+//if wrong score goes down and it shows you poekmon & name removes from remaining pokemon array
+//skip goes to next pokemon
+//hint display a hint to the name of the pokemon
+//reset resets score, resets kept track of pokemon
